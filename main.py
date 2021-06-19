ddff979cb223b8f7c7ef1159e4787c7350da132c
galjeza <61537604+galjeza@users.noreply.github.com>
2021-06-19 09:06:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ def kopirajInPrilepiPodatke(url):
         print("")
     #spremeni podatek zato da nebo isti oglas v arhivu
     randoma = str(random.randint(1998, 2021))
-    randomc = str(int(driver.find_element_by_name("cena").text) + int(driver.find_element_by_name("cena").text)*0.05)
+    randomc = str(int(driver.find_element_by_name("cena").text) + 500)
     
     randomc = str(random.randint(50000, 100000))
     driver.find_element_by_name("letoReg").click()
