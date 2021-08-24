ce8f0b5dc7c848835e452092308c18e80eaf5de3
galjeza <61537604+galjeza@users.noreply.github.com>
2021-08-24 14:32:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -106,7 +106,12 @@ def ustvariNovOglasStran():
     time.sleep(1)       
     Select(driver.find_element_by_name("oblika")).select_by_index(0)
     time.sleep(1)
-    Select(driver.find_element_by_name("mesec")).select_by_value(mesReg)
+    try:
+        Select(driver.find_element_by_name("mesec")).select_by_value(mesReg)
+    except:
+        Select(driver.find_element_by_name("mesec")).select_by_value("Novo")                  
+           
+    
     time.sleep(1)
     Select(driver.find_element_by_name("leto")).select_by_visible_text(letoReg)
     time.sleep(1)
@@ -353,6 +358,13 @@ def main():
     login(email, geslo)
     driver.get("https://www.avto.net/_2016mojavtonet/results.asp?znamka=&model=&modelID=&tip=&znamka2=&model2=&tip2=&znamka3=&model3=&tip3=&cenaMin=0&cenaMax=999999&letnikMin=0&letnikMax=2090&bencin=0&starost2=999&oblika=0&ccmMin=0&ccmMax=99999&mocMin=&mocMax=&kmMin=0&kmMax=9999999&kwMin=0&kwMax=999999&motortakt=0&motorvalji=0&lokacija=0&sirina=0&dolzina=&dolzinaMIN=0&dolzinaMAX=100&nosilnostMIN=0&nosilnostMAX=999999&lezisc=&presek=0&premer=0&col=0&vijakov=0&EToznaka=0&vozilo=&airbag=&barva=&barvaint=&EQ1=1000000000&EQ2=1000000000&EQ3=1000000000&EQ4=100000000&EQ5=1000000000&EQ6=1000001000&EQ7=1110100122&EQ8=1010000001&EQ9=1000000000&KAT=1010000000&PIA=&PIAzero=&PSLO=&akcija=0&paketgarancije=&broker=11084&prikazkategorije=0&kategorija=0&zaloga=10&arhiv=0&presort=3&tipsort=DESC&stran=1&subSORT=3&subTIPSORT=ASC")
     results = driver.find_elements_by_class_name("ResultsAd")
+    try:
+     driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/div[2]/form/div[104]/ul/a[1]/li").click()
+     resultstwo = driver.find_elements_by_class_name("ResultsAd")
+     for result in resultstwo:
+           results.append(result)
+    except:
+        pass
     for result in results :
         try:
             result.find_element_by_class_name("ResultsAdPriceRegular")
