3a70a90c81294ff171ba6feb4e0784a791d7bfda
galjeza <61537604+galjeza@users.noreply.github.com>
2021-12-17 13:27:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 abeceda = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
            'w', 'x', 'y', 'z']
-rabiHeadless = true;
+rabiHeadless = "";
 headers = {
     'Accept-Encoding': 'gzip, deflate, sdch',
     'Accept-Language': 'en-US,en;q=0.8',
