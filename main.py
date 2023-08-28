5cb5a81c397ae0b44f87d1d266458b3e582da41e
Gal Jeza <61537604+galjeza@users.noreply.github.com>
2023-08-28 22:26:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -405,7 +405,7 @@ def main():
                
                       
 
-    driver = webdriver.Chrome(ChromeDriverManager(version=chromeVersion).install(), chrome_options=chrome_options)
+    driver = webdriver.Chrome(chrome_options=chrome_options)
     print("=> vsi gonilniki uspešno pridobljeni")
 
     driver.get("https://www.avto.net/_2016mojavtonet/")
